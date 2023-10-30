c8c6053dad4b24f32ad61bcc3f061717cd0b784f
Cristian Bautista <criistiian1596@gmail.com>
2023-10-30 10:51:26 -0300

    Version Desplegable v2.1

--- src/routes/reservas.routes.js
+++ src/routes/reservas.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { logMiddleware } from '../middleware/log.middleware.js';
+import { checkJwt } from '../middleware/check.middleware.js';
 import { getAllReservations_ctrl } from '../controllers/reservas.controller.js'
 
 const router = Router();
 
-router.get('/calendario', logMiddleware, getAllReservations_ctrl);
+router.get('/calendario', logMiddleware, checkJwt, getAllReservations_ctrl);
 
 export default router;
