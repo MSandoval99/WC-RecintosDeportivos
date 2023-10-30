4fd9389e3141d030d51b01af5bb46afdd7cfb528
Cristian Bautista <criistiian1596@gmail.com>
2023-10-29 23:33:49 -0300

    Version Desplegable v2.1

--- src/services/reservas.services.js
+++ src/services/reservas.services.js
@@ -3,9 +3,10 @@ import { InternalServerError } from '../utils/error.handle.js';
 
 export const getAllReservations = async () => {
     try {
-        const [reservas] = await pool.query('SELECT * FROM Reserva');
+        const query = 'SELECT * FROM Reserva WHERE Semana_id_semana = 3';
+        const [reservas] = await pool.query(query);
         return reservas;
     } catch (error) {
-        throw new InternalServerError('Error al obtener reservas.');
+        throw new InternalServerError('Error al obtener reservas de la semana 3.');
     }
 };
